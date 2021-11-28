3e373b0be205d39153d5d11c926bece2ec7b71c1
tmarriaga19 <tmarriaga@gmail.com>
2021-11-27 20:31:29 -0500

    Commit Pagina Inicial

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,25 @@
-function Footer (){
-    return(
-       <footer className="bg-primary text-white mt-5">
-        <div className="container">
-          <div className="card bg-primary">
-            <div className="card-body">
-              <h2>Texaco Colombia</h2>
-              <h6 className="mb-3">Le damos combustible a tus sueños</h6>
-              <p className="mb-1">Dirección: Carrera 56 # 19-95</p>
-              <p className="mb-1">Teléfono: (1) 447 3910</p>
-              <p className="mb-1">Bogotá - Colombia</p>
-              
+function Footer() {
+  return (
+    <footer className="bg-primary text-white mt-5">
+      <div className="container">
+        <div className="card bg-primary">
+          <div className="card-body">
+            <h2>Texaco Colombia</h2>
+            <h6 className="mb-3">Le damos combustible a tus sueños</h6>
+            <p className="mb-1">Dirección: Carrera 56 # 19-95</p>
+            <p className="mb-1">Teléfono: (1) 447 3910</p>
+            <p className="mb-1">Bogotá - Colombia</p>
+            <div className="container">
+              <i className="bi bi-facebook"></i>
+              <i className="bi bi-instagram m-3"></i>
+              <i className="bi bi-twitter"></i>
+
             </div>
           </div>
         </div>
-      </footer>
-    );
+      </div>
+    </footer>
+  );
 
 }
 
